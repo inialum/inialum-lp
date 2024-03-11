765d14ad873978f9c6377f9dc5d15b01648f2da2
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-12 00:15:36 +0900

    chore: Add tailwind.css and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,35 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   site: 'https://inialum.org',
   trailingSlash: 'always',
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
   },
+  vite: {
+    resolve: [
+      {
+        find: '@Root',
+        replacement: resolve(__dirname, '.'),
+      },
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  }
 })
