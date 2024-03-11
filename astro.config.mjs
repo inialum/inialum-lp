251a8c328261d3617ecba35707bf94a8074ea5c6
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-11 23:14:35 +0900

    chore: Update config for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+  site: 'https://inialum.org',
+  trailingSlash: 'always',
   integrations: [react(), tailwind()],
   i18n: {
     defaultLocale: 'ja',
