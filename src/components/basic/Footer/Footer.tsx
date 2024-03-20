f7862977603dbb3c41e4e5931348449d02e2b2a7
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-21 01:29:52 +0900

    feat: Add SNS info to Footer

--- src/components/basic/Footer/Footer.tsx
+++ src/components/basic/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { type ComponentPropsWithRef } from 'react'
 
 import styles from './Footer.module.css'
 
+import xImage from '@/assets/images/common/x_logo.png'
 import logoImage from '@/assets/logo/inialum_logo_light_transparent@3x.png'
 
 type Props = ComponentPropsWithRef<'footer'>
@@ -38,7 +39,17 @@ export const Footer = ({ className, ...rest }: Props) => {
         </ul>
       </nav>
       <div className={styles.Footer__Bottom}>
-        <div className={styles.Footer__Item}>
+        <div className={styles['Footer__Item--top']}>
+          <a href="https://twitter.com/inialum" target="_blank" rel="noopener">
+            <img
+              src={xImage.src}
+              alt="X公式アカウント"
+              width={24}
+              height={24}
+            />
+          </a>
+        </div>
+        <div className={styles['Footer__Item--small']}>
           <a
             href="https://inialum.notion.site/4cbf67b190544bff8891deabc64e776c"
             target="_blank"
@@ -53,8 +64,11 @@ export const Footer = ({ className, ...rest }: Props) => {
           >
             プライバシーポリシー
           </a>
+          <a href="https://github.com/inialum" target="_blank" rel="noopener">
+            GitHub
+          </a>
         </div>
-        <div className={styles.Footer__Item}>
+        <div className={styles['Footer__Item--small']}>
           &copy; {nowYear} INIALUM - INIAD Alumni Meetings.
         </div>
       </div>
